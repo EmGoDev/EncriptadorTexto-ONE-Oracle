d0af088b2ccff07f0713a2ae0e6eb1d0e662d1cf
Emiliano Gonzalez <93420074+EmGoAR@users.noreply.github.com>
2022-08-12 20:21:19 -0300

    encoding and Uncoding working!

--- main/main.js
+++ main/main.js
@@ -5,13 +5,12 @@ input.focus()
 resultText = document.querySelector(".resultCard p")
 //console.log(resultText.innerHTML)
 
-let botonEncriptar = document.querySelector("button")
-
+let botonEncriptar = document.querySelector(".botonEncriptar")
+let botonDesencriptar = document.querySelector(".botonDesencriptar")
 
 function encriptar() {
     resultado = ""
     for (let i = 0; i < input.value.length; i++) {
-        console.log("iterando: " + input.value[i])
         if (input.value[i] == "a") { resultado += (input.value[i].replace("a", "ai")) }
         else if (input.value[i] == "e") { resultado += (input.value[i].replace("e", "enter")) }
         else if (input.value[i] == "i") { resultado += (input.value[i].replace("i", "imes")) }
@@ -22,12 +21,32 @@ function encriptar() {
     //termino de ckeckear, se reinicia el input
     input.value = ""
     //resultado
-    console.log("resultado: \n" + resultado)
+    return resultado
+}
+
+function desencriptar() {
+    resultado = ""
+    for (let i = 0; i < input.value.length; i++) {
+        input.value = input.value.replace("ai", "a")
+        input.value = input.value.replace("enter", "e")
+        input.value = input.value.replace("imes", "i")
+        input.value = input.value.replace("ober", "o")
+        input.value = input.value.replace("ufat", "u")
+        resultado = input.value
+    }
+    //termino de ckeckear, se reinicia el input
+    input.value = ""
+    //resultado
     return resultado
 }
 
 botonEncriptar.onclick = () => {
     resultado = encriptar()
+    resultText.innerHTML = resultado
+}
+
+botonDesencriptar.onclick = () => {
+    resultado = desencriptar()
     resultText.innerHTML = resultado
 }
 
