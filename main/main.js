1d8cf946320c8531bef6eeb93bbff66fba80e2d7
EmGoAR <emicba07@gmail.com>
2022-08-12 16:29:02 -0300

    PC UI finished with basic functionality

--- main/main.js
+++ main/main.js
@@ -1,2 +1,16 @@
-let input = document.querySelector("inputUsuario")
+
+let input = document.querySelector("textarea")
 input.focus()
+
+resultText = document.querySelector(".resultCard p")
+console.log(resultText.innerHTML)
+
+let botonEncriptar = document.querySelector("button")
+
+botonEncriptar.onclick = () => {
+    resultText.innerHTML = input.value
+    input.value = ""
+}
+
+
+
