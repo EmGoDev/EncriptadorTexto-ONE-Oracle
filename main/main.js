90df7e4add5c531bf89be11815113aa3b83796af
Emiliano Gonzalez <93420074+EmGoAR@users.noreply.github.com>
2022-08-12 19:46:58 -0300

    finiched PC UI and encoding logic

--- main/main.js
+++ main/main.js
@@ -3,14 +3,32 @@ let input = document.querySelector("textarea")
 input.focus()
 
 resultText = document.querySelector(".resultCard p")
-console.log(resultText.innerHTML)
+//console.log(resultText.innerHTML)
 
 let botonEncriptar = document.querySelector("button")
 
-botonEncriptar.onclick = () => {
-    resultText.innerHTML = input.value
+
+function encriptar() {
+    resultado = ""
+    for (let i = 0; i < input.value.length; i++) {
+        console.log("iterando: " + input.value[i])
+        if (input.value[i] == "a") { resultado += (input.value[i].replace("a", "ai")) }
+        else if (input.value[i] == "e") { resultado += (input.value[i].replace("e", "enter")) }
+        else if (input.value[i] == "i") { resultado += (input.value[i].replace("i", "imes")) }
+        else if (input.value[i] == "o") { resultado += (input.value[i].replace("o", "ober")) }
+        else if (input.value[i] == "u") { resultado += (input.value[i].replace("u", "ufat")) }
+        else { resultado += (input.value[i]) }
+    }
+    //termino de ckeckear, se reinicia el input
     input.value = ""
+    //resultado
+    console.log("resultado: \n" + resultado)
+    return resultado
 }
-//intentando hace un cambio cualquiera para borrar el theme jekyyl de mierda
+
+botonEncriptar.onclick = () => {
+    resultado = encriptar()
+    resultText.innerHTML = resultado
+}
 
 
